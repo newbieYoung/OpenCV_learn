1ab436707e367f4ac025a025e121ab129c4db639
NewYoung <657851574@qq.com>
2019-08-24 14:42:46 +0800

    getMinMaxChannels

--- demo/tool.js
+++ demo/tool.js
@@ -10,3 +10,28 @@ function getChannels(mat, row, col) {
   }
   return channel;
 }
+
+/**
+ * 获取图像中各通道的最小值和最小值
+ */
+function getMinMaxChannels(mat) {
+  let mins = [];
+  let maxs = [];
+  let len = mat.channels();
+  let planes = new cv.MatVector()
+  cv.split(mat, planes)
+
+  for (let i = 0; i < len; i++) {
+    let channel = planes.get(i);
+    let loc = cv.minMaxLoc(channel)
+    mins.push(getChannels(channel, loc.minLoc.y, loc.minLoc.x)[0]);
+    maxs.push(getChannels(channel, loc.maxLoc.y, loc.maxLoc.x)[0]);
+  }
+
+  planes.delete();
+
+  return {
+    mins: mins,
+    maxs: maxs
+  };
+}
